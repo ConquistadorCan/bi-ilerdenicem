a432db90bf74eafc457aeea87b578cd089936a07
ConquistadorCan <canmustafafatih@gmail.com>
2024-06-01 19:07:12 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,38 @@
+const http = require('http')
+const fs = require('fs')
+const socketIo = require('socket.io')
+
+const port = 5000
+const server = http.createServer((req, res) => {
+  if (req.url === '/') {
+    fs.readFile(__dirname + '/index.html', (err, data) => {
+      if (err) {
+        res.writeHead(500)
+        res.end('Error loading index.html')
+      } else {
+        res.writeHead(200, { 'Content-Type': 'text/html' })
+        res.end(data)
+      }
+    })
+  }
+})
+
+const io = socketIo(server, {
+  cors: {
+    // Replace with your client-side URL
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST']
+  }
+})
+
+io.on('connection', (socket) => {
+  console.log('A user connected')
+
+  socket.on('send message', (chat) => {
+    io.emit('send message', chat)
+  })
+})
+
+server.listen(port, () => {
+  console.log(`Server is listening at the port: ${port}`)
+})
